3935e09d2c4d083c365534d9ebd18d250431f0ec
sunheng <sunhengtaing@gmail.com>
2014-02-01 20:49:48 -0500

    Adjusted Login UI layout

--- gen/com/example/teamjavatar/R.java
+++ gen/com/example/teamjavatar/R.java
@@ -27,8 +27,8 @@ public final class R {
         public static final int action_settings=0x7f080005;
         public static final int button1=0x7f080002;
         public static final int button2=0x7f080003;
-        public static final int editText1=0x7f080000;
-        public static final int editText2=0x7f080001;
+        public static final int editText1=0x7f080001;
+        public static final int editText2=0x7f080000;
         public static final int textView1=0x7f080004;
     }
     public static final class layout {
@@ -43,8 +43,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int email_field=0x7f050006;
         public static final int hello_world=0x7f050005;
         public static final int login=0x7f050002;
+        public static final int password_field=0x7f050007;
         public static final int register=0x7f050003;
         public static final int title_activity_login_controls=0x7f050004;
     }
