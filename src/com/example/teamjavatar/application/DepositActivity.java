f987ea2f223ad38fe7bb029e90e34b87f0af0be6
sunheng <sunhengtaing@gmail.com>
2014-03-06 19:14:38 -0500

    added deposit form page

--- src/com/example/teamjavatar/application/DepositActivity.java
+++ src/com/example/teamjavatar/application/DepositActivity.java
@@ -6,7 +6,13 @@ import com.example.teamjavatar.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class DepositActivity extends Activity {
 
@@ -22,5 +28,22 @@ public class DepositActivity extends Activity {
 		getMenuInflater().inflate(R.menu.deposit, menu);
 		return true;
 	}
-
+	
+	public void deposit(View view){
+		EditText source = (EditText) findViewById(R.id.transactionName);
+		EditText amount = (EditText) findViewById(R.id.amount);
+		DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker);
+		int day = datePicker.getDayOfMonth();
+		int month = datePicker.getMonth() + 1;
+		int year = datePicker.getYear();
+//		String accountName = an.getText().toString();
+//		String displayName = dn.getText().toString();
+//		String ir = in.getText().toString();
+		CharSequence errorMessage = month + " ";
+		Toast errorToast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+		errorToast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 0);
+		errorToast.show();
+//		Intent intent = new Intent(this, AccountHistoryActivity.class);
+//    	startActivity(intent);
+	}
 }
