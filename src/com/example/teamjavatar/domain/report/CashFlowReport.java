0b29daf3a7e00e32db84a6cf8ed541e26d862b71
Brian Dong <dong.brian@gatech.edu>
2014-03-13 00:24:11 -0400

    skeleton for spendingreports

--- src/com/example/teamjavatar/domain/report/CashFlowReport.java
+++ src/com/example/teamjavatar/domain/report/CashFlowReport.java
@@ -4,6 +4,11 @@ package com.example.teamjavatar.domain.report;
 
 public class CashFlowReport extends Report {
 
+	public CashFlowReport(String userID) {
+		super(userID);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
