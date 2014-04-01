ada4629bfa374d3c03aa64dab75410fa43df168b
Surya Bhamidipati <suryavenkatram@gmail.com>
2014-03-31 22:23:58 -0400

    spaces and tabs

--- src/com/example/teamjavatar/application/AccountHistoryActivity.java
+++ src/com/example/teamjavatar/application/AccountHistoryActivity.java
@@ -18,80 +18,83 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class AccountHistoryActivity extends Activity {
 
-	private TransactionDAO transactionDataSource;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_account_history);
-		
-		transactionDataSource = new TransactionDAO(this);
-		transactionDataSource.open();
-		
-		setListView();
-	}
+    private TransactionDAO transactionDataSource;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.account_history, menu);
-		return true;
-	}
-	
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		transactionDataSource.close();
-	}
-	
-	@Override
-	protected void onNewIntent(Intent intent) {
-		super.onNewIntent(intent);
-		setListView();
-		setIntent(intent);
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		setListView();
-	}
-	
-	public void gotoWithdrawal(View view){
-		Intent intent = new Intent(this, WithdrawalActivity.class);
-    	startActivityForResult(intent, 0);
-	}
-	
-	public void gotoDeposit(View view){
-		Intent intent = new Intent(this, DepositActivity.class);
-    	startActivityForResult(intent, 0);
-	}
-	
-	public void manageTransaction(View view, AbstractTransaction transaction) {
-		UserApplication app = (UserApplication) getApplication();
-		app.setTransaction(transaction);
-		Intent intent = new Intent(this, TransactionManagementActivity.class);
-		startActivityForResult(intent, 0);
-	}
-	
-	public void setListView() {
-		UserApplication app = (UserApplication) this.getApplication();
-		int accountID = app.getAccount().getID();
-		List<AbstractTransaction> transactions = transactionDataSource.getTransactionsList(accountID); 
-		ListView listView = (ListView) findViewById(R.id.listview);
-		ArrayAdapter<AbstractTransaction> adapter = new ArrayAdapter<AbstractTransaction>(this, R.layout.list_item, R.id.listItemTextView, transactions);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_history);
+
+        transactionDataSource = new TransactionDAO(this);
+        transactionDataSource.open();
+
+        setListView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.account_history, menu);
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        transactionDataSource.close();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setListView();
+        setIntent(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        setListView();
+    }
+
+    public void gotoWithdrawal(View view) {
+        Intent intent = new Intent(this, WithdrawalActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    public void gotoDeposit(View view) {
+        Intent intent = new Intent(this, DepositActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    public void manageTransaction(View view, AbstractTransaction transaction) {
+        UserApplication app = (UserApplication) getApplication();
+        app.setTransaction(transaction);
+        Intent intent = new Intent(this, TransactionManagementActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    public void setListView() {
+        UserApplication app = (UserApplication) this.getApplication();
+        int accountID = app.getAccount().getID();
+        List<AbstractTransaction> transactions = transactionDataSource
+                .getTransactionsList(accountID);
+        ListView listView = (ListView) findViewById(R.id.listview);
+        ArrayAdapter<AbstractTransaction> adapter = new ArrayAdapter<AbstractTransaction>(
+                this, R.layout.list_item, R.id.listItemTextView, transactions);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new OnListItemClickListener());
         listView.setClickable(true);
-	}
-	
-	private class OnListItemClickListener implements OnItemClickListener {
+    }
 
-		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position,
-				long id) {
-			AbstractTransaction transaction = (AbstractTransaction) parent.getItemAtPosition(position);
-			manageTransaction(view, transaction);
-		}
-	}
+    private class OnListItemClickListener implements OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                long id) {
+            AbstractTransaction transaction = (AbstractTransaction) parent
+                    .getItemAtPosition(position);
+            manageTransaction(view, transaction);
+        }
+    }
 
 }
