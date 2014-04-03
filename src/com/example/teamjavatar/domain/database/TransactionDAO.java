c4bf5e6c4507317c8673e8935a963d9f52ccf420
sunheng <sunhengtaing@gmail.com>
2014-04-03 00:05:47 -0400

    UserDAO and TransactionDAO checkstyle completed

--- src/com/example/teamjavatar/domain/database/TransactionDAO.java
+++ src/com/example/teamjavatar/domain/database/TransactionDAO.java
@@ -15,25 +15,46 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
+/**
+ * Access to transaction table.
+ * @author Team Javatar
+ *
+ */
 public class TransactionDAO {
+    /** Set up database connection. */
     private SQLiteDatabase database;
+    /** Set up database and access table/columns. */
     private SQLHelper dbHelper;
 
+    /**
+     * Constructor.
+     * @param context Context of the application
+     */
     public TransactionDAO(Context context) {
         dbHelper = new SQLHelper(context);
     }
 
+    /**
+     * Open connection to the database.
+     * @throws SQLException cannot establish connection with database
+     */
     public void open() throws SQLException {
         database = dbHelper.getWritableDatabase();
     }
 
+    /** Close database connection. */
     public void close() {
         dbHelper.close();
     }
 
+    /**
+     * Get list of transaction from accountID.
+     * @param accountID Account ID
+     * @return A list of transaction
+     */
     public List<AbstractTransaction> getTransactionsList(int accountID) {
         List<AbstractTransaction> list = new ArrayList<AbstractTransaction>();
-        String[] where = { String.valueOf(accountID) };
+        String[] where = {String.valueOf(accountID) };
         Cursor cursor = database.rawQuery("SELECT * FROM "
                 + SQLHelper.TABLE_TRANSACTION + " WHERE "
                 + SQLHelper.COLUMN_ACCOUNTID + " = ? ", where);
@@ -47,22 +68,45 @@ public class TransactionDAO {
         return list;
     }
 
+    /**
+     * Get a list of deposits.
+     * @return A list of deposits
+     */
     public List<Deposit> getDepositsList() {
         // TODO implement this method
         return null;
     }
 
+    /**
+     * Get a list of deposits with userID and fromDate.
+     * @param userID UserID
+     * @param fromDate FromDate
+     * @param toDate ToDate
+     * @return A list of deposit with those conditions
+     */
     public List<Deposit> getDepositsList(String userID, long fromDate,
             long toDate) {
         // TODO implement this method
         return null;
     }
 
+    /**
+     * Get a list of withdrawals for a userID.
+     * @param userID UserID
+     * @return A list of withdrawals
+     */
     public List<Withdrawal> getWithdrawalsList(String userID) {
         // TODO implement this method
         return null;
     }
 
+    /**
+     * Get a list of withdrawals given userID, fromDate and toDate.
+     * @param userID UserID
+     * @param fromDate FromDate
+     * @param toDate ToDate
+     * @return A list of withdrawals according to specification
+     */
     public List<Withdrawal> getWithdrawalsList(String userID, long fromDate,
             long toDate) {
         List<Withdrawal> list = new LinkedList<Withdrawal>();
@@ -73,10 +117,10 @@ public class TransactionDAO {
                 + SQLHelper.COLUMN_EFFECTIVETIMESTAMP + " BETWEEN ? AND ? "
                 + " AND a." + SQLHelper.COLUMN_ACCOUNTID + " = t."
                 + SQLHelper.COLUMN_ACCOUNTID + " AND t."
-                + SQLHelper.COLUMN_AMOUNT + " < 0 ", new String[] { userID,
-                String.valueOf(fromDate), String.valueOf(toDate) });
-        if (cursor.getCount() <= 0)
+                + SQLHelper.COLUMN_AMOUNT + " < 0 ", new String[] {userID, String.valueOf(fromDate), String.valueOf(toDate)});
+        if (cursor.getCount() <= 0) {
             return list;
+        }
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
             AbstractTransaction trans = cursorToTransaction(cursor);
@@ -86,6 +130,13 @@ public class TransactionDAO {
         return list;
     }
 
+    /**
+     * Add deposit to account ID.
+     * @param accountID AccountID
+     * @param transName Transaction Name
+     * @param efDate Effective Date
+     * @param amount Amount
+     */
     public void addDeposit(int accountID, String transName, long efDate,
             double amount) {
         ContentValues tran = new ContentValues();
@@ -99,6 +150,14 @@ public class TransactionDAO {
         database.insert(SQLHelper.TABLE_TRANSACTION, null, tran);
     }
 
+    /**
+     * Add a withdrawal for account ID into the database.
+     * @param accountID AccountID
+     * @param transName Transaction Name
+     * @param efDate Effective Date
+     * @param amount Amount  
+     * @param category Category
+     */
     public void addWithdrawal(int accountID, String transName, long efDate,
             double amount, String category) {
         ContentValues tran = new ContentValues();
@@ -114,8 +173,13 @@ public class TransactionDAO {
 
     }
 
+    /**
+     * Get data from query to a transaction object.
+     * @param cursor The tuple from the query
+     * @return Transaction object containing information from database
+     */
     private AbstractTransaction cursorToTransaction(Cursor cursor) {
-        int ID = cursor.getInt(cursor.getColumnIndex(SQLHelper.COLUMN_TRANSID));
+        int id = cursor.getInt(cursor.getColumnIndex(SQLHelper.COLUMN_TRANSID));
         long effectiveTimestamp = cursor.getLong(cursor
                 .getColumnIndex(SQLHelper.COLUMN_EFFECTIVETIMESTAMP));
         long enteredTimestamp = cursor.getLong(cursor
@@ -127,17 +191,18 @@ public class TransactionDAO {
         String name = cursor.getString(cursor
                 .getColumnIndex(SQLHelper.COLUMN_TRANSNAME));
         boolean isCommited;
-        if (committed == 0)
+        if (committed == 0) {
             isCommited = false;
-        else
+        } else {
             isCommited = true;
+        }
         if (amount > 0) {
-            return new Deposit(ID, name, enteredTimestamp, effectiveTimestamp,
+            return new Deposit(id, name, enteredTimestamp, effectiveTimestamp,
                     amount, isCommited);
         } else {
             String category = cursor.getString(cursor
                     .getColumnIndex(SQLHelper.COLUMN_CATEGORY));
-            return new Withdrawal(ID, name, enteredTimestamp,
+            return new Withdrawal(id, name, enteredTimestamp,
                     effectiveTimestamp, -amount, isCommited, category);
         }
     }
--- src/com/example/teamjavatar/domain/database/UserDAO.java
+++ src/com/example/teamjavatar/domain/database/UserDAO.java
@@ -10,26 +10,37 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
-/*
+/**
  * single responsibility principle
  * only handles access to user table in database
  * 
  * command query separation principle
- * queries do not affect the database
+ * queries do not affect the database.
  */
 public class UserDAO {
 
+    /** Set up database connection. */
     private SQLiteDatabase database;
+    /** Set up database and access table/columns. */
     private SQLHelper dbHelper;
 
+    /**
+     * Constructor.
+     * @param context Context of the application
+     */
     public UserDAO(Context context) {
         dbHelper = new SQLHelper(context);
     }
 
+    /**
+     * Open connection to the database.
+     * @throws SQLException cannot establish connection with database
+     */
     public void open() throws SQLException {
         database = dbHelper.getWritableDatabase();
     }
 
+    /** Close database connection. */
     public void close() {
         dbHelper.close();
     }
@@ -38,20 +49,23 @@ public class UserDAO {
      * Registers the specified user as a new user if the userID does not already
      * exist.
      * 
-     * @param user
+     * @param userID UserID
+     * @param password Password
+     * @param firstName First name
+     * @param lastName Last name
      *            the user object containing the new user's information
      * @return true if the registration was successful, false if the user id
      *         already exists
-     * @throws Exception
      */
     public boolean registerUser(String userID, String password,
             String firstName, String lastName) {
-        String[] where = new String[] { userID };
+        String[] where = new String[] {userID};
         Cursor cursor = database.rawQuery("SELECT * FROM "
                 + SQLHelper.TABLE_USERS + " WHERE " + SQLHelper.COLUMN_USERID
                 + " = ?", where);
-        if (cursor.getCount() == 1)
+        if (cursor.getCount() == 1) {
             return false;
+        }
         ContentValues values = userInfoToValues(userID, password, firstName,
                 lastName);
         database.insert(SQLHelper.TABLE_USERS, null, values);
@@ -63,46 +77,64 @@ public class UserDAO {
      * if the user ID and password do not match, otherwise returns a user or
      * admin based on the user ID.
      * 
-     * @param userID
-     * @param password
-     * @return
+     * @param userID User ID
+     * @param password Password
+     * @return User containing information from database
      */
     public AbstractUser getUser(String userID, String password) {
-        String[] where = new String[] { userID, password };
+        String[] where = new String[] {userID, password};
         Cursor cursor = database.rawQuery("SELECT * FROM "
                 + SQLHelper.TABLE_USERS + " WHERE " + SQLHelper.COLUMN_USERID
                 + " = ? AND " + SQLHelper.COLUMN_PASSWORD + " = ?", where);
-        if (cursor.getCount() == 0)
+        if (cursor.getCount() == 0) {
             return null;
+        }
         if (userID.equals("admin")) {
             return new Admin();
         }
         cursor.moveToFirst();
         return cursorToUser(cursor);
-        // return new User(userID, "ASDf", "ASDf");
     }
 
+    /**
+     * Change password of a user except admin.
+     * @param userID User ID 
+     * @param password New Password
+     */
     public void changeUserPassword(String userID, String password) {
         // do not change if userID == "admin"
         // TODO implement this method
     }
 
-    private ContentValues userIDToValues(String userID) {
-        String[] where = new String[] { userID };
-        Cursor cursor = database.rawQuery("SELECT * FROM "
-                + SQLHelper.TABLE_USERS + " WHERE " + SQLHelper.COLUMN_USERID
-                + " = ?", where);
-        String password = cursor.getString(cursor
-                .getColumnIndex(SQLHelper.COLUMN_PASSWORD));
-        String firstName = cursor.getString(cursor
-                .getColumnIndex(SQLHelper.COLUMN_FIRSTNAME));
-        String lastName = cursor.getString(cursor
-                .getColumnIndex(SQLHelper.COLUMN_LASTNAME));
-        ContentValues values = userInfoToValues(userID, password, firstName,
-                lastName);
-        return values;
-    }
+//    /**
+//     * Create content to insert to the database.
+//     * @param userID UserID
+//     * @return The content from the database
+//     */
+//    private ContentValues userIDToValues(String userID) {
+//        String[] where = new String[] {userID};
+//        Cursor cursor = database.rawQuery("SELECT * FROM "
+//                + SQLHelper.TABLE_USERS + " WHERE " + SQLHelper.COLUMN_USERID
+//                + " = ?", where);
+//        String password = cursor.getString(cursor
+//                .getColumnIndex(SQLHelper.COLUMN_PASSWORD));
+//        String firstName = cursor.getString(cursor
+//                .getColumnIndex(SQLHelper.COLUMN_FIRSTNAME));
+//        String lastName = cursor.getString(cursor
+//                .getColumnIndex(SQLHelper.COLUMN_LASTNAME));
+//        ContentValues values = userInfoToValues(userID, password, firstName,
+//                lastName);
+//        return values;
+//    }
 
+    /**
+     * Create content to insert to the database.
+     * @param userID UserID
+     * @param password Password
+     * @param firstName First name
+     * @param lastName Last name
+     * @return The content to put into the database
+     */
     private ContentValues userInfoToValues(String userID, String password,
             String firstName, String lastName) {
         ContentValues values = new ContentValues();
@@ -113,6 +145,11 @@ public class UserDAO {
         return values;
     }
 
+    /**
+     * Get the user object from database tuple.
+     * @param cursor Tuple returned by database
+     * @return The user object
+     */
     private User cursorToUser(Cursor cursor) {
         String userID = cursor.getString(cursor
                 .getColumnIndex(SQLHelper.COLUMN_USERID));
